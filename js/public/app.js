9640b0c980f38273f692dd79d5a13a55857be074
Hendrik Leppelsack <hendrik@leppelsack.de>
2015-07-10 12:57:44 +0200

    add word counter

--- js/public/app.js
+++ js/public/app.js
@@ -272,6 +272,17 @@ app.directive('notesTooltip', function () {
     };
 });
 
+app.filter('wordCount', function () {
+	'use strict';
+	return function (value) {
+		if (value && (typeof value === 'string')) {
+			return value.trim().split(/\s+/).length;
+		} else {
+			return 0;
+		}
+	};
+});
+
 app.factory('Config', ["Restangular", function (Restangular) {
     'use strict';
 
